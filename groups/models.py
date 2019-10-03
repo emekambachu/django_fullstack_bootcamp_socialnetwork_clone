28410ed24bd6dc7f16b650f3afc7d854e366d962
Chukwuemeka Mbachu <victor.c.mbachu@gmail.com>
2019-10-03 18:20:52 +0100

    could not install misaka

--- groups/models.py
+++ groups/models.py
@@ -1,13 +1,6 @@
-from django.db import models
-
 # for url slugs and pretty urls
-from django.utils.text import slugify
 
 # Groups app
 # Create your models here.
 
-#run pip install django-misaka first
-
-
-
-
+# run pip install --only-binary :all: mysqlclient first
