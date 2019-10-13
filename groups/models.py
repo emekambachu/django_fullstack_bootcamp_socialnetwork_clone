bbb4ef92ae7680b5bcddc9dcd3babb1a88c9fe52
dexmbachu <xeddtech@gmail.com>
2019-10-13 13:31:04 +0100

    working on group model

--- groups/models.py
+++ groups/models.py
@@ -26,9 +26,16 @@ class Group(models.Model):
     slug = models.SlugField(allow_unicode=True, unique=True)
     description = models.TextField(blank=True, default='')
     description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify()
 
 
-class GroupMembers(models.Model):
+class GroupMember(models.Model):
     group = models.ForeignKey(Group, related_name='memberships')
     user = models.ForeignKey(User, related_name='user_groups')
 
